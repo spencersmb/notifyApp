d547c416b46676c825372b8c8a8bfb2dd9f9a8de
spencersmb <spencer.bigum@gmail.com>
2016-03-16 21:17:40 -0400

    first commit
    #1

---
+++ app/app.ts
@@ -0,0 +1,50 @@
+import {App, IonicApp, Platform} from 'ionic-angular';
+import {GettingStartedPage} from './pages/getting-started/getting-started';
+import {ListPage} from './pages/list/list';
+
+
+@App({
+  templateUrl: 'build/app.html',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+})
+class MyApp {
+  rootPage: any = GettingStartedPage;
+  pages: Array<{title: string, component: any}>
+
+  constructor(private app: IonicApp, private platform: Platform) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Getting Started', component: GettingStartedPage },
+      { title: 'List', component: ListPage }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // The platform is now ready. Note: if this callback fails to fire, follow
+      // the Troubleshooting guide for a number of possible solutions:
+      //
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      //
+      // First, let's hide the keyboard accessory bar (only works natively) since
+      // that's a better default:
+      //
+      // Keyboard.setAccessoryBarVisible(false);
+      //
+      // For example, we might change the StatusBar color. This one below is
+      // good for dark backgrounds and light text:
+      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    let nav = this.app.getComponent('nav');
+    nav.setRoot(page.component);
+  }
+}
